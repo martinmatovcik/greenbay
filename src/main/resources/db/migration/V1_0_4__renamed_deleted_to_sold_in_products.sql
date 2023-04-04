09e43b68455643358e9d02e93262256c6b576005
Martin Maťovčík <116836668+martinmatovcik@users.noreply.github.com>
2023-04-04 11:12:53 +0200

    feat: flyway set-up improved

--- src/main/resources/db/migration/V1_0_4__renamed_deleted_to_sold_in_products.sql
+++ src/main/resources/db/migration/V1_0_4__renamed_deleted_to_sold_in_products.sql
@@ -1,2 +1,19 @@
 ALTER TABLE products
     CHANGE COLUMN deleted sold BOOLEAN NOT NULL DEFAULT FALSE;
+
+ALTER TABLE products
+    ADD COLUMN user_id BIGINT NOT NULL DEFAULT 0,
+    ADD CONSTRAINT users_fk FOREIGN KEY (user_id) REFERENCES users (id);
+
+CREATE TABLE IF NOT EXISTS bids
+(
+    id         BIGINT   NOT NULL AUTO_INCREMENT,
+    value      INT      NOT NULL,
+    created_at DATETIME NOT NULL,
+    product_id BIGINT,
+    user_id    BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
