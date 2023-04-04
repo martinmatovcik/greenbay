fb96b8155f3e2ff0b4834e419a1ed80acec7d901
Martin Maťovčík <116836668+martinmatovcik@users.noreply.github.com>
2023-04-04 11:27:07 +0200

    feat: User <---> Bid relationship

--- src/main/java/com/gfa/greenbay/entities/GreenbayUser.java
+++ src/main/java/com/gfa/greenbay/entities/GreenbayUser.java
@@ -1,143 +1,148 @@
 package com.gfa.greenbay.entities;
 
 import com.gfa.greenbay.entities.enums.Role;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Objects;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
 public class GreenbayUser implements UserDetails {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(unique = true)
-  private String username;
+    @Column(unique = true)
+    private String username;
 
-  private String email;
-  private String password;
+    private String email;
+    private String password;
 
-  @Enumerated(EnumType.STRING)
-  private Role role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-  private Integer balance;
+    private Integer balance;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Product> products;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Bid> bids;
 
-  public GreenbayUser() {
-  }
-
-  public GreenbayUser(String username, String email, String password, Role role) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.role = role;
-    this.balance = 0;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public Role getRole() {
-    return role;
-  }
-
-  public void setRole(Role role) {
-    this.role = role;
-  }
-
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
-
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Integer getBalance() {
-    return balance;
-  }
-
-  public void setBalance(Integer balance) {
-    this.balance = balance;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    public GreenbayUser() {
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    GreenbayUser user = (GreenbayUser) o;
-    return Objects.equals(id, user.id)
-        && Objects.equals(username, user.username)
-        && Objects.equals(email, user.email)
-        && Objects.equals(password, user.password)
-        && role == user.role
-        && Objects.equals(balance, user.balance);
-  }
 
-  @Override
-  public int hashCode() {
-    return 0;
-  }
+    public GreenbayUser(String username, String email, String password, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.balance = 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GreenbayUser user = (GreenbayUser) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username)
+                && Objects.equals(email, user.email) && Objects.equals(password,
+                user.password) && role == user.role && Objects.equals(balance, user.balance)
+                && Objects.equals(products, user.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
